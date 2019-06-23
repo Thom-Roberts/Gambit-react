19e7162381bed63a1a0495a6ad7535feb614a97c
Thom-Roberts <robertsorange@gmail.com>
2019-06-23 12:40:35 -0600

    Added production credentials

--- src/services/BUNGIECRED.js
+++ src/services/BUNGIECRED.js
@@ -1,4 +1,5 @@
 var exports = module.exports = {};
 
-exports.BUNGIEAPIKEY = 'fc470e42da39445380152053f1a86267';
+//exports.BUNGIEAPIKEY = 'fc470e42da39445380152053f1a86267'; // development
+exports.BUNGIEAPIKEY = '7771c372dbe34060a7bbb10b3016ecfe'; // production
 
